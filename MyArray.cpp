3f3b9f3b086a45ddce64216642262d6e1bc64ffd
Mosmahmoud <94164308+Mosmahmoud@users.noreply.github.com>
2021-11-17 14:06:35 +0200

    mostafa

--- MyArray.cpp
+++ MyArray.cpp
@@ -1,102 +1,148 @@
 #include "MyArray.h"
-
-template<class T>
+#include <iostream>
+using namespace std;
+template<class T> 
 MyArray<T>::MyArray(){
-   size = 0;
-   capacity = INITIAL_CAPACITY ;
-   data = new T[capacity];
-}
-template<class T>
-MyArray<T>::MyArray(T*arr ,int size){
-    this->size = size;
-    capacity = size;
-    data = new T[capacity];
-    for (int i = 0; i<size ;++i){
-        data[i] = arr[i];
-    } 
+        size = 0 ;
+        capacity = INITIAL_CAPACITY;
+        data = new T [capacity];
     
 }
-template<class T>
-T MyArray<T>::get(int index){
-    return nullptr;
 
-}
-template<class T>
-void MyArray<T>::push(T element){
-   if(size == capacity)
-   {
-       capacity *=2;
-       T *temp = new T[capacity];
-        for (int i = 0 ; i<size ;++i)
-        {
-           temp[i] = data[i];
-        }
-      delete []data;
-      data = temp;
-      }
-   
-   data[size] = element;
-   size++;                  
-}
-template<class T>
-T MyArray<T>::pop()
-{   
-  if(size >= 0)
-  {  
-  
-   
-     size--;
-    if(size == capacity/4)
-    {
-       capacity /=2;
-       T *temp = new T[capacity]
-        for (int i = 0 ; i<size ;++i)
-        {
-           temp[i] = data[i];
-        }
+
+template<class T> MyArray<T>::MyArray(T *array, int size_array){
+    size = size_array;
+    if (size_array >= capacity){
+        capacity *= 2;
     }
-    delete []data;
-    data = temp;
-  } 
-  return -1;
-}
-template<class T>
-void MyArray<T>::print(){
-   
-   for(int i =0; i<size ; ++i )
-   {
-      std::cout<<data[i]<<' ';
-     
-   }
-  std::cout<<std::endl;
-}
-template<class T>
-int MyArray<T>::get_capacity(){
-    return capacity;
-}
-template<class T>
-void MyArray<T>::insert(int index , T element)
-{
-      if(index < size)
-      {
-          data[size]=element;
-      } 
-    if(index == size)
-    {
-        push(element);
-    }   
-}
-template<class T>
-T MyArray<T>::remove(int index)
-{
-   for(int i=size ; i>index ;--i)
-   {
-       data[i-1]=data[i];
-   }
-}
-template<class T>
-int MyArray<T>::get_size()
-{
-    return size;
+    
+    data = new T[capacity];
+        for (int i=0; i<size_array;i++){
+                data[i]= array[i];
+           }
 }
 
+
+
+
+template<class T> T MyArray<T>::get(int index){
+if (index <size)
+return data[index];
+else {
+    return -1;
+}
+}
+
+
+
+
+template<class T> void MyArray<T>::push(T element){
+    if (size == capacity ){
+        capacity *=2;
+        T *temp = new T[capacity];
+        for (int i = 0;i<size;i++)
+        temp[i] = data[i];
+    
+delete data;
+data = temp;
+}
+
+data [size] = element;
+size++;
+
+}
+
+
+
+
+template<class T> T MyArray<T>::pop(){
+    if (size ==0){
+        return -1;
+    }
+T j = data [size -1 ];
+    size -- ;
+
+if (size == capacity/4){
+    capacity/=2;
+      T *temp = new T[capacity];
+        for (int i = 0;i<size;i++)
+        temp[i] = data[i];
+    
+delete []data;
+data = temp;
+}
+return j;
+}
+
+
+
+template<class T> void MyArray<T>::insert(int index, T element){
+    if (index == capacity){
+        capacity *=2;
+        push(element);
+    }
+    else {
+        if (index == capacity);
+        capacity *= 2;
+        T *temp = new T[capacity];
+        for (int i = 0; i < size; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+    for (int i = (size+1);i>=index;i--){
+        data[i]=data[i-1];
+    }
+    data [index]=element;
+    size++;
+}
+
+
+
+template<class T> T MyArray<T>::remove(int index){
+   T *tep = new T [capacity];
+   int j=0;
+    T x=data[index];
+    if (index >= size){
+        return -1 ;
+    }
+    if (index < size){
+    for (int i = 0 ;i<size;i++){
+        if (i==index){
+            j++;
+
+         }
+         else{
+             tep[i-j]=data[i];
+         }
+    }
+delete[] data;
+data = tep;
+size--;
+if (size == capacity/4){
+    capacity/=2;
+      T *temp = new T[capacity];
+        for (int i = 0;i<size;i++)
+        temp[i] = data[i];
+    
+delete [] data;
+data = temp;
+}
+return x;}
+    else {
+        return -1;
+    }
+}
+template<class T> void MyArray<T>::print(){
+for (int i=0 ; i <size; i++){
+    cout<< data[i]<< ' ' ;
+}
+cout<<endl;
+}
+template<class T> int MyArray<T>::get_size(){
+    return size;
+
+}
+template<class T> int MyArray<T>::get_capacity(){
+return capacity;
+}
--- main.cpp
+++ main.cpp
@@ -2,22 +2,22 @@
 #include "MyArray.h"
 using namespace std;
 
-int main(int argc, char** argv){
-    // driving code
+//int main(int argc, char** argv){
+     // driving code
     
-     int arr[] = {5, 3, 2};
-     MyArray<int>* my_arr = new MyArray<int>(arr, 3);
-     my_arr->push(5);
-     my_arr->push(5);
-     my_arr->push(5);
-     my_arr->push(5);
-     my_arr->push(5);
-     my_arr->push(5);
-     my_arr->push(5);
-     cout << my_arr->get_capacity() << endl;
-     my_arr->print();
+     //int arr[] = {5, 3, 2};
+     //MyArray<int>* my_arr = new MyArray<int>(arr, 3);
+     //my_arr->push(5);
+     //my_arr->push(5);
+     //my_arr->push(5);
+     //my_arr->push(5);
+     //my_arr->push(5);
+     //my_arr->push(5);
+     //my_arr->push(5);
+     //cout << my_arr->get_capacity() << endl;
+      //  my_arr->print(); 
 
-
+ 
      //MyArray<int>* my_arr = new MyArray<int>();
      //my_arr->push(5);
      //my_arr->insert(0, 10);
@@ -26,4 +26,4 @@ int main(int argc, char** argv){
      //int arr[] = {5, 3, 2};
      //MyArray<int>* my_arr = new MyArray<int>(arr, 3);
      //cout << my_arr->remove(1) << " " << my_arr->pop() << " " << my_arr->remove(0) << endl;
-}
+//}
